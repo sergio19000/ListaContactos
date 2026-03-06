3bf626640dd7d8304b4d9dc849ffa75f8fd72a6c
sergio19000 <119664418+sergio19000@users.noreply.github.com>
2026-03-06 10:37:49 +0000

    cambios pequeños

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,11 @@ export const Navbar = () => {
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1">Lista de Contactos</span>
 				</Link>
 				<div className="ml-auto">
 					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+						<button className="btn btn-primary">Create New Contact</button>
 					</Link>
 				</div>
 			</div>
--- src/pages/Demo.jsx
+++ src/pages/Demo.jsx
@@ -8,31 +8,16 @@ export const Demo = () => {
 
   return (
     <div className="container">
-      <ul className="list-group">
-        {/* Map over the 'todos' array from the store and render each item as a list element */}
-        {store && store.todos?.map((item) => {
-          return (
-            <li
-              key={item.id}  // React key for list items.
-              className="list-group-item d-flex justify-content-between"
-              style={{ background: item.background }}> 
-              
-              {/* Link to the detail page of this todo. */}
-              <Link to={"/single/" + item.id}>Link to: {item.title} </Link>
-              
-              <p>Open file ./store.js to see the global store that contains and updates the list of colors</p>
-              
-              <button className="btn btn-success" 
-                onClick={() => dispatch({
-                  type: "add_task", 
-                  payload: { id: item.id, color: '#ffa500' }
-                })}>
-                Change Color
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+      <div className="hoja">
+        <label for="nombre">Nombre</label>
+        <input type="text" id="nombre" name="nombre"></input>
+
+         <label for="nombre">Nombre</label>
+        <input type="text" id="nombre" name="nombre"></input>
+
+         <label for="nombre">Nombre</label>
+        <input  id="nombre" name="nombre"></input>
+      </div>
       <br />
 
       <Link to="/">
